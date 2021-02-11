b20604ddfc8dbb025aca4c6ce9cc37dbdb47cf21
Razei <razeixyz@gmail.com>
2021-02-11 15:36:36 -0500

    Updated build settings

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -17,8 +17,8 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         XRotation -= mouseY;
         XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
