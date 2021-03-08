f73e9ce2269f3a83e60ef8be3451c591a53288ef
Alvin <asjyap29@hotmail.com>
2021-03-08 00:19:24 -0500

    Changed Main Character + Added MC Animations

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+
+    public enum PlayerState
+    {
+        IDLE,
+        RUN,
+        JUMP,
+        KICK
+    }
+
+    private Animator animator;
+
     public CharacterController controller;
+    [Header("Movement")]
     public float maxSpeed = 10.0f;
     public float gravity = -30.0f;
     public float jumpHeight = 3.0f;
+    [Header("Ground Detection")]
     public Transform groundCheck;
     public float groundRadius = 0.5f;
+    public bool isGrounded;
     public LayerMask groundMask;
     public Vector3 velocity;
-    public bool isGrounded;
+   
     private PauseMenu pauseMenu;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         pauseMenu = GameObject.Find("Canvas").GetComponent<PauseMenu>();
     }
@@ -25,11 +40,13 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetInteger("AnimState", (int)PlayerState.IDLE);
         if (pauseMenu.Paused)
         {
             return;
         }
 
+      
         isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -44,8 +61,19 @@ public class PlayerBehaviour : MonoBehaviour
 
         controller.Move(move * maxSpeed * Time.deltaTime);
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("AnimState", (int)PlayerState.RUN);
+        }
+        else {
+            animator.SetInteger("AnimState", (int)PlayerState.IDLE);
+        }
+        
+
         if (Input.GetButton("Jump") && isGrounded)
         {
+            Debug.Log("jumping");
+            animator.SetInteger("AnimState", (int)PlayerState.JUMP);
             velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
         }
 
@@ -56,7 +84,7 @@ public class PlayerBehaviour : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.white;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
     }
 }
